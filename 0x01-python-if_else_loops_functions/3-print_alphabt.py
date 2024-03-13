cb36250fd4725ac857ec10746f5ab89d64f7f1fa
Your Name <you@example.com>
2024-03-13 08:01:39 +0000

    Readme

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for ascii_value in range( 97, 123):
+    # Convert ASCII value to character
+    letter = chr(ascii_value)
+    # Check if letter is not 'q' and not 'e'
+    if letter != 'q' and letter != 'e':
+        # Print the letter without newline
+        print(letter, end='')
