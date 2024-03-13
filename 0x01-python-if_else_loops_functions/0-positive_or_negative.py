31874fbc25b535bf813ad36781cd40e99995fb44
Your Name <you@example.com>
2024-03-13 05:13:33 +0000

    Readme

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-# YOUR CODE HERE
-if number  > 0:
-    print(number, "is positive")
-elif number == 0:
-    print(number, "is negative")
-else:
-    print(number, "is negative",' \n')
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+
+print(number, "is" + (" positive" if number > 0 else (" zero" if number == 0 else " negative")))
