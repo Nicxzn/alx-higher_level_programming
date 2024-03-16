e283e1fe3979c3e8939b06ddc8b213fe04413233
Your Name <you@example.com>
2024-03-16 02:04:01 +0000

    Readme

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for number in range(99):
-    print(f"{number:02d}", end=",")
-print(f"{number + 1:d}")
+for a in range(0, 99):
+    print("{:02d}".format(a), end=", ")
+print("{:d}".format(a + 1))
