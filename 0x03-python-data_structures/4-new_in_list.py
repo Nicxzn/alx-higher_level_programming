1bbdc92b3be3e4b78064ad2780031f9798567500
Your Name <you@example.com>
2024-03-19 01:52:07 +0000

    readme

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    new_string=" "
-    for char in my_string:
-        if char !='c' and char !='C':
-            new_string+=char
-            return new_string
+def new_in_list(my_list, idx, element):
+    if idx >=0 or idx < len(my_list):
+        new_list= my_list[:]
+        new_list[idx]= element
+        return new_list
+    
+    
+    
+original_list = [1, 2, 3, 4, 5]
+idx = 2
+new_element = 10
+updated_list = new_in_list(original_list, idx, new_element)
+print("Original list:", original_list)
+print("Updated list:", updated_list)
