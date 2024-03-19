dabdeca519470c6b495a70c3ee9f654cf12350ac
Your Name <you@example.com>
2024-03-19 01:41:43 +0000

    readme

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string=" "
+    new_string=""
     for char in my_string:
         if char is not 'c' and char is not 'C':
-            new_string+=char
-            return new_string
+            new_string+= char
+    return new_string
+        
