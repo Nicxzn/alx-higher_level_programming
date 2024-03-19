625630bcc96cca8fc1841ad65694707c73c8a16d
Your Name <you@example.com>
2024-03-19 00:51:41 +0000

    Readme

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list:
         for num in reversed(my_list):
-            print("{}".format(num))
+            print("{:d}".format(num))
 
     
